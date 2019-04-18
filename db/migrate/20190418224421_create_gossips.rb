ea356f09484197bdfc9bf846a99773a0ca92e034
Yousri <laifayousri@gmail.com>
2019-04-19 00:50:29 +0200

    Mises en relation des gossips et users

--- db/migrate/20190418224421_create_gossips.rb
+++ db/migrate/20190418224421_create_gossips.rb
@@ -1,6 +1,8 @@
 class CreateGossips < ActiveRecord::Migration[5.1]
   def change
     create_table :gossips do |t|
+      t.belongs_to :user, index: true
+
       t.string :title
       t.text :content
 
