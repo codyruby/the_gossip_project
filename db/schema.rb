da49b0ff07f7a2d1ed1f449600cd9d83b36df71a
Yousri <laifayousri@gmail.com>
2019-04-18 19:55:05 +0200

    Ajout de la table cities et du champ city_id dans la table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190418172727) do
+ActiveRecord::Schema.define(version: 20190418174550) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.string "zip_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +27,8 @@ ActiveRecord::Schema.define(version: 20190418172727) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
 
 end
